95e6f018ee3be4d1a0ccf6c920a0e385606bcea7
Simon <simon@onedotsix.com>
2022-11-01 15:16:11 +0100

    Font glyph fixes for ZH + VI locales

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -28,7 +28,7 @@ class MyDocument extends Document {
 
 					<link rel="preconnect" href="https://fonts.googleapis.com"/>
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-					<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
+					<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Noto+Serif+TC:wght@900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
 				</Head>
 				<body>
 					<Main />
