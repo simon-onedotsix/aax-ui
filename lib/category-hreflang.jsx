1cfa9e64fe8aaf0af8a85f32476e783c324ef3db
Simon <simon@onedotsix.com>
2022-11-12 17:56:55 +0100

    Added pagination params back into hreflang lib

--- lib/category-hreflang.jsx
+++ lib/category-hreflang.jsx
@@ -4,11 +4,16 @@ export const handleHrefLangLinks = ( router ) => {
 
     // console.log('current locales: ', router.locales)
     // console.log('current locale: ', router.locale)
-    
     // console.log('router:', router)
     // console.log('path:', `/category/${router.query.category}`)
 
-    let path = `/category/${router.query.category}`
+    let path
+
+    if (router.query.page) {
+        path = `/category/${router.query.category}?page=${router.query.page}`
+    } else {
+        path = `/category/${router.query.category}`
+    }
 
     return (
         <>
@@ -18,7 +23,14 @@ export const handleHrefLangLinks = ( router ) => {
 
             {
                 router.locales.map( locale => {
-                    return <link key={locale} href={`https://trends.aax.com${locale !== 'en' ? `/${locale}` : ''}${path}`} rel="alternate" hrefLang={locale}/>
+                    return (
+                        <link 
+                            key={locale} 
+                            href={`https://trends.aax.com${locale !== 'en' ? `/${locale}` : ''}${path}`} 
+                            rel="alternate" 
+                            hrefLang={locale}
+                        />
+                    )
                 })
             }
         </>
